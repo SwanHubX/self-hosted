3464071f8fd2329ac90b76c79db8d40e2e1f8788
hyYY-1024 <75209465+hyYY-1024@users.noreply.github.com>
2025-04-27 15:54:41 +0800

    update Readme and path
    更新readme文件并只修改swanlab下的docker-compose.yaml

--- docker/upgrade.sh
+++ docker/upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # update docker-compose.yaml swanlab.* image version
 
@@ -13,8 +13,15 @@ add_replica_env() {
             }
         }
     }
-    ' docker-compose.yaml
+    ' swanlab/docker-compose.yaml
 }
+
+# check docker-compose.yaml exists
+if [ ! -f "swanlab/docker-compose.yaml" ]; then
+    echo "docker-compose.yaml not found, please run install.sh directly"
+    exit 1
+fi
+
 # confirm information
 read -p "Updating the container version will restart docker-compose. Do you agree? [y/N] " confirm
 
@@ -25,13 +32,13 @@ if [[ "$confirm" == [yY] || "$confirm" == [yY][eE][sS] ]]; then
     /^[[:space:]]+image: .*swanlab.*:v1$/ {
         s/(:v1)$/\1.1/
     }
-    ' docker-compose.yaml
+    ' swanlab/docker-compose.yaml
     add_replica_env
     # delete backup
-    rm -f docker-compose.yaml.bak
+    rm -f swanlab/docker-compose.yaml.bak
 
     # restart docker-compose
-    docker-compose pull && docker-compose up -d
+    docker-compose -f swanlab/docker-compose.yaml pull && docker-compose -f swanlab/docker-compose.yaml up -d
     echo "finish update"
 else
     echo "update canceled"
